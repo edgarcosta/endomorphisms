f3140d4b162b190a4019c88eed5ea50b99f4e917
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-04-27 14:34:52 -0400

    better error message

--- endomorphisms/__init__.py
+++ endomorphisms/__init__.py
@@ -19,7 +19,7 @@ try:
     magma.AttachSpec(__magmapath__ + 'spec')
 except RuntimeError as err:
     # the err comes with a endline
-    print("RuntimeError exception caught:\n%s\nWe weren't able to load the magma package and some functionality of the endomorphisms package will be limited." % str(err).rstrip('\n'))
+    print("RuntimeError exception caught, with error message:\n\t > %s\nWe weren't able to load the magma package and some functionality of the endomorphisms package will be limited." % str(err).rstrip('\n'))
 
 
 from Curves import mHyperellipticCurve, mPlaneCurve
